eddbe1ad804bd409373326d8e8d9171089059f70
Leonid Shagabutdinov <leonid@shagabutidnov.com>
2013-10-08 19:51:55 +0200

    all opened views in current window now used for building completion list

--- anyword_completion.py
+++ anyword_completion.py
@@ -5,9 +5,6 @@ import anyword_completion_helper
 class AnywordCompletionCommand( sublime_plugin.TextCommand ):
 
   def run( self, edit, direction = 'asc' ):
-
-    # self.view.insert( edit, 0, 'test' )
-
     if not hasattr( self, 'completion_helper' ):
       self.completion_helper = anyword_completion_helper.AnywordCompletion()
       self.completion_state = self.completion_helper.create_empty_state()
@@ -15,20 +12,33 @@ class AnywordCompletionCommand( sublime_plugin.TextCommand ):
     self.edit = edit
 
     cursor_position = self.view.sel()[0].b
-    text = self.view.substr( sublime.Region( 0, self.view.size() ) )
+    window = self.view.window()
+
+    texts = []
+    for view_index, view in enumerate( window.views() ):
+      texts.append( view.substr( sublime.Region( 0, view.size() ) ) )
+
+    text_index = window.get_view_index( window.active_view() )[1]
+
     desc = direction == 'desc'
+    self.insert_or_update_completion( cursor_position, texts, text_index,
+      desc )
 
-    self.insert_or_update_completion( cursor_position, text, desc )
+  def insert_or_update_completion( self, cursor_position, texts, text_index, desc ):
+    completion = self.completion_helper.get_completion( texts, text_index,
+      cursor_position, desc, self.completion_state )
 
-  def insert_or_update_completion( self, cursor_position, text, desc ):
-    completion = self.completion_helper.get_completion( [ text ], 0, cursor_position, desc, self.completion_state )
     self.completion_state = completion['state']
 
     if completion['completion'] is None:
       return None
 
     edit = self.view.begin_edit()
-    # self.view.insert( edit, self.view.size(), "\n\n" + str( self.completion_state ) + "\n\n" )
-    self.view.erase( edit, sublime.Region( completion['change_start_position'], completion['change_end_position'] ) )
-    self.view.insert( edit, completion['change_start_position'], completion['completion'] )
+
+    self.view.erase( edit, sublime.Region( completion['change_start_position'],
+      completion['change_end_position'] ) )
+
+    self.view.insert( edit, completion['change_start_position'],
+      completion['completion'] )
+
     self.view.end_edit( edit )
